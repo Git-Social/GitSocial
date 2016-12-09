97bd167adf1156cac6109e480cd5e1f465b60985
Justin Bennett <jbennett@scrippsnetworks.com>
2016-12-08 21:23:19 -0500

    Add new deployment method, upgrade dependencies

--- vue.config.js
+++ vue.config.js
@@ -1,12 +1,27 @@
 module.exports = {
   title: 'GitSocial',
   resolve: true,
+  publicPath: '/GitSocial/',
   mergeConfig: {
     resolve: {
       extensions: ['.styl']
     },
-    stylus: {
-      preferPathResolver: 'webpack'
+    module: {
+      rules: [
+        {
+          test: /\.styl$/,
+          use: [
+            'style-loader',
+            'css-loader',
+            {
+              loader: 'stylus-loader',
+              options: {
+                preferPathResolve: 'webpack'
+              }
+            }
+          ]
+        }
+      ]
     }
   }
 }
